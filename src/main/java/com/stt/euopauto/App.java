074e1845d795b67fd06e251832ae7964e48361ab
norstc <norstc@gmail.com>
2018-09-18 15:38:27 +0800

    upgrade to junit 4

--- src/main/java/com/stt/euopauto/App.java
+++ src/main/java/com/stt/euopauto/App.java
@@ -1,5 +1,9 @@
 package com.stt.euopauto;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Result result = JUnitCore.runClasses(AppTest.class);
+        
+        if(result.wasSuccessful()) {
+        	System.out.println("well done");
+        }else {
+        	System.out.println("some thing wrong...");
+        	for (Failure failure : result.getFailures()) {
+            	System.out.println(failure.toString());
+            	System.out.println(failure.getMessage());
+            }
+        }
+        
     }
 }
